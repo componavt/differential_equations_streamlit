66707d05491f7e8372ca0cf189b7c04187b640af
Andrew Krizhanovsky <andrew.krizhanovsky@gmail.com>
2025-07-01 17:52:51 +0300

    003_solutions_gene_ODU_unique.npz

--- gene_regulatory_ODE_system.py
+++ gene_regulatory_ODE_system.py
@@ -5,7 +5,7 @@ import matplotlib.cm as cm
 
 # Load saved data from .npz file (structured arrays)
 @st.cache_data
-def load_data(path="data/solutions_gene_ODU.npz"):
+def load_data(path="data/003_solutions_gene_ODU_unique.npz"):
     npzfile = np.load(path, allow_pickle=True)
     data = []
     for i in range(len(npzfile['alpha'])):
