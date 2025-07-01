ccb43a421f7f0f666c3273fe2d0e6885d808cd85
Andrew Krizhanovsky <andrew.krizhanovsky@gmail.com>
2025-07-01 14:27:23 +0300

    wo comments

--- gene_regulatory_ODE_system.py
+++ gene_regulatory_ODE_system.py
@@ -1,15 +1,3 @@
-"""
-Gene Regulatory ODE System – Streamlit App
-
-Explore numerical solutions to a system of ODEs modeling gene regulation
-using different solvers and parameter settings.
-
-🔗 Live App: https://neuraldiffur.streamlit.app/
-📦 Source code: https://github.com/componavt/differential_equations_streamlit
-
-Author: Andrew Krizhanovsky
-"""
-
 import streamlit as st
 import pickle
 import numpy as np
