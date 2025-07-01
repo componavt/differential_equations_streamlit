d349a8a456bd37c3e1c809099857fd576e1d7d52
Andrew Krizhanovsky <andrew.krizhanovsky@gmail.com>
2025-07-01 14:52:45 +0300

    without latex

--- gene_regulatory_ODE_system.py
+++ gene_regulatory_ODE_system.py
@@ -81,12 +81,6 @@ if st.button("Start Animation"):
 
 # Sliders and explanation after the plots
 st.markdown("---")
-st.latex(r"""
-\begin{cases}
-\frac{dx}{dt} = \frac{K\,x^{1/\alpha}}{b^{1/\alpha} + x^{1/\alpha}} \;\-\; \gamma_1\,x,\\[6pt]
-\frac{dy}{dt} = \frac{K\,y^{1/\alpha}}{b^{1/\alpha} + y^{1/\alpha}} \;\-\; \gamma_2\,y.
-\end{cases}
-""")
 
 st.markdown("Select parameters to animate solutions for each method across alpha values.")
 
