24f52f85541a2669be08c6d31a21d84d1b7f1f93
Andrew Krizhanovsky <andrew.krizhanovsky@gmail.com>
2025-07-04 09:55:58 +0300

    correct formulae gene_regulatory_ODE_system.py

--- gene_regulatory_ODE_system.py
+++ gene_regulatory_ODE_system.py
@@ -83,8 +83,8 @@ st.markdown("---")
 st.markdown("**System of ODEs:**")
 st.latex(r"""
 \begin{cases}
-\frac{dx}{dt} = \frac{K\,x^{1/\alpha}}{b^{1/\alpha} + x^{1/\alpha}} \;\-\; \gamma_1\,x,\\[6pt]
-\frac{dy}{dt} = \frac{K\,y^{1/\alpha}}{b^{1/\alpha} + y^{1/\alpha}} \;\-\; \gamma_2\,y.
+\frac{dx}{dt} = \frac{K\,x^{1/\alpha}}{b^{1/\alpha} + x^{1/\alpha}} - \gamma_1\,x,\\[6pt]
+\frac{dy}{dt} = \frac{K\,y^{1/\alpha}}{b^{1/\alpha} + y^{1/\alpha}} - \gamma_2\,y.
 \end{cases}
 """)
 st.markdown("""
